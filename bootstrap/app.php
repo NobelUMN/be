9c3df068ddc63ffa8faa424c6cbf54e6fe306e75
Palhacoo <147512829+Palhacoo@users.noreply.github.com>
2025-12-10 00:23:33 +0700

    fix: add HandleCors middleware to api routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->api(append: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
+
         $middleware->statefulApi();
         
         $middleware->alias([
